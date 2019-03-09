a3dc49599b63f8b4250bf2981336f496496d1d54
IgnacioVilches <felipe.vilches.belloni@gmail.com>
2019-03-08 21:46:39 -0300

    se fixeo la linea 7 de dinosaurio.rb

---
+++ dinosaurio.rb
@@ -0,0 +1,25 @@
+class Dinosaurio
+    def initialize (raza = "eran reptiles")
+    def initialize (tiempo = "hace muchos años")
+
+        end
+            end
+                
+end
+  
+class Lagartija < Dinosaurio
+
+  
+end
+
+class Tortuga < Dinosaurio
+
+  
+end
+
+class Cocodrilo < Dinosaurio
+
+end
+
+
+
